e018188d6198ee53243110a7f3e42297363fb5df
Ahmadov Jeyhun <jeyhundm@gmail.com>
2025-08-18 23:06:07 +0300

    fix: ValidationBehavior

--- GymManagement.Application/Behaviors/ValidationBehavior.cs
+++ GymManagement.Application/Behaviors/ValidationBehavior.cs
@@ -9,15 +9,20 @@ public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TReques
     where TRequest : IRequest<TResponse>
     where TResponse : IErrorOr
 {
-    private readonly IValidator<TRequest> _validator;
+    private readonly IValidator<TRequest>? _validator;
 
-    public ValidationBehavior(IValidator<TRequest> validator)
+    public ValidationBehavior(IValidator<TRequest>? validator = null)
     {
         _validator = validator;
     }
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
+        if (_validator is null)
+        {
+            return await next();
+        }
+
         ValidationResult? validationResult = await _validator.ValidateAsync(request, cancellationToken);
 
         if (validationResult.IsValid)
